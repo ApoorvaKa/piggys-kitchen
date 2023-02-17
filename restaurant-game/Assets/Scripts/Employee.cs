73c5b5e9530f308ce5bdbcfb276c8dad4555adcb
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-02-17 11:51:52 -0500

    Initial Commit

---
+++ restaurant-game/Assets/Scripts/Employee.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee : MonoBehaviour
+{
+
+    public string job;
+    public string name;
+    public string background;
+
+    public int desired_salary;
+    public int salary;
+    public int skill_rating;
+    public int gold_multiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
