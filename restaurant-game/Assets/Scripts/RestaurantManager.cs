8d5f8d1a1f9249edff04f76473b4abe7584e3df9
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-02-17 12:00:41 -0500

    Initial Restaurant Manager

---
+++ restaurant-game/Assets/Scripts/RestaurantManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestaurantManager : MonoBehaviour
+{
+    public int gold;
+    public int gold_multiplier;
+    public int gold_per_second;
+
+    // public list of employees
+    public List<Employee> employees;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
