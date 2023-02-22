34bac719ccc1889a1eeb5f2798049c5bfbd0c4b0
William Yang <wy2037@nyu.edu>
2023-02-22 11:01:36 -0500

    Restart Screen + Small Fixes

--- restaurant-game/Assets/Scripts/RestaurantManager.cs
+++ restaurant-game/Assets/Scripts/RestaurantManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class RestaurantManager : MonoBehaviour
 {
@@ -11,12 +12,12 @@ public class RestaurantManager : MonoBehaviour
     public int round = 1;
     int dailyCost = 10, startingGold = 0;
 
-    public GameObject background, dayComplete;
+    public GameObject background, dayComplete, gameOver;
     GameObject player;
 
     public Image timerBar;
     public ResourceManager upgrade;
-    public TextMeshProUGUI moneyText, goldEarned, dailyCosts, totalGold, roundText;
+    public TextMeshProUGUI moneyText, goldEarned, dailyCosts, totalGold, roundText, roundEnd;
     public TextMeshProUGUI[] itemTexts;
     public SubmitDish orders;
     public string[] itemNames;
@@ -42,17 +43,27 @@ public class RestaurantManager : MonoBehaviour
         roundText.text = "Round " + round.ToString();
         if (currentTime > 0 && player.GetComponent<Player>().canMove == true) {
             currentTime -= Time.deltaTime;
+            if (currentTime/maxTime > 0.5f) {
+                timerBar.color = new Color(0,1,0);
+            } else if (currentTime/maxTime > 0.25f) {
+                timerBar.color = new Color(1,1,0);
+            } else {
+                timerBar.color = new Color(1,0,0);
+            }
             timerBar.fillAmount = currentTime / maxTime;
         } else {
             Debug.Log("Time");
             player.GetComponent<Player>().canMove = false;
+            if (money - dailyCost < 0) {
+                totalGold.color = new Color(1, 0, 0);
+            }
             totalGold.text = money.ToString();
             if (currentTime <= 0) {
                 background.SetActive(true);
                 dayComplete.SetActive(true);
                 dailyCost = (int)Mathf.Pow(2f, (float)round-1f) * 10;
                 dailyCosts.text = dailyCost.ToString();
-                goldEarned.text = (money-startingGold).ToString() + "+" + startingGold.ToString();
+                goldEarned.text = startingGold.ToString() + "+" + (money-startingGold).ToString();
                 money -= dailyCost;
             }
             currentTime = maxTime + (upgrade.PatienceLevelNum * 20);
@@ -69,6 +80,13 @@ public class RestaurantManager : MonoBehaviour
             orders.Randomize(round);
         } else {
             Debug.Log("GameOver");
+            gameOver.SetActive(true);
+            roundEnd.text = "You made it to round " + round.ToString();
+            dayComplete.SetActive(false);
         }
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(0);
     }
 }
